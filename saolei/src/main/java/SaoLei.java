10e4a3c1aa5ebd08c3c5ef70722af27e4fcfaf17
yuanhuimu <yuanhuimu@creditease.cn>
2019-11-12 18:51:01 +0800

    visible

--- saolei/src/main/java/SaoLei.java
+++ saolei/src/main/java/SaoLei.java
@@ -4,14 +4,19 @@ public class SaoLei {
 
     JFrame frame = new JFrame("扫雷游戏");
 
-    public static void main(String[] args){
-        SaoLei lei = new SaoLei();
-    }
     //构造函数
     public SaoLei(){
         frame.setSize(600,700);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+
+        SaoLei lei = new SaoLei();
     }
 }
 
